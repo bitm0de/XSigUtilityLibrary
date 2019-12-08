f4cb325078d6137c5389c966477c6616dc650166
bitm0de <aceofspin@hotmail.com>
2019-12-07 19:19:20 -0700

    Updated assembly information.

--- XSigUtility/Properties/AssemblyInfo.cs
+++ XSigUtility/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@
 [assembly: AssemblyTitle("XSigUtility")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("XSigUtility")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyCopyright("Copyright © Troy Garner 2019")]
+[assembly: AssemblyVersion("1.0.*")]
 
